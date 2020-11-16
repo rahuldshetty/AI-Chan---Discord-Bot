10e63bc169640dbd3cf73c1d72d827b976826636
Rahul <35rahuldshetty@gmail.com>
2020-11-16 22:47:05 +0530

    added support for rasa

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 import discord
 from message_handlers.memes import send_random_meme
 from message_handlers.utility import check_valid_url
+from message_handlers.rasa_chat import get_chat_message
 
 TOKEN = os.getenv('DISCORD_TOKEN')
 
@@ -26,17 +27,20 @@ async def on_message(message):
     # this bot should only respond to messages with ayaya
     if  "ayaya" in msg:
         if "meme" in msg:
-            url = send_random_meme()
+            entities = msg.split()
+            msg = ""
+            if entities >= 3:
+                msg = entities[2]
+            url = send_random_meme(msg)
             if check_valid_url(url):
                 await message.channel.send(url)
             else:
                 await message.channel.send("No AYAYA Found :(")
         else:
-            url = send_random_meme("AYAYA")
-            if check_valid_url(url):
-                await message.channel.send(url)
-            else:
-                await message.channel.send("AYAYAYA AYAYAYA AYAYAYA AYAYAYA")
-        
+            msg_list = msg.split()
+            msg_list.remove("ayaya")
+            response = get_chat_message(" ".join(msg_list))
+            await message.channel.send(response)
+
 
 client.run(TOKEN)
---
+++ message_handlers/rasa_chat.py
@@ -0,0 +1,28 @@
+import requests
+import logging
+
+RASA_AI_ENDPOINT = "https://rasa-ai-chan.herokuapp.com/webhooks/rest/webhook"
+
+def get_chat_message(message="Hi"):
+    payload = {
+        "message": message
+    }
+
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    try:
+        response = requests.post(RASA_AI_ENDPOINT, data=payload, headers=headers)
+        results = response.json()
+        result = ""
+
+        for answer in results:
+            result += answer['text'] + "\n"
+        
+        return result
+
+
+    except Exception as err:
+        logging.error(err)
+        return "UwU!"
+    
